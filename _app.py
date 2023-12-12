3462b597a9da132fb03588cc0955165027cd9ece
Casper Audenaert <casper.audenaert@student.hogent.be>
2023-12-12 20:21:10 +0100

    update

--- _app.py
+++ _app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, send_file
 import pandas as pd
-from surprise import Dataset, Reader, KNNBasic
-from sklearn.metrics import average_precision_score, mean_squared_error, mean_absolute_error, precision_score, recall_score, f1_score, roc_auc_score
-import numpy as np
+from surprise import Dataset, Reader, SVD, KNNBasic
 
 app = Flask(__name__)
 
@@ -14,10 +12,11 @@ def index():
 def recommendations():
     if request.method == 'POST':
         user_id = request.form['user_id']
-
+        
         df = pd.read_csv('test.csv')
         full = df
-        df = df[['crm_Contact_Contactpersoon', 'crm_Campagne_Campagne', 'Similarity', 'crm_Campagne_Naam_in_email']]
+        df = df[['crm_Contact_Contactpersoon', 'crm_Campagne_Campagne', 'Similarity','crm_Campagne_Naam_in_email']]
+
 
         reader = Reader(rating_scale=(0, 10))
         data = Dataset.load_from_df(df[['crm_Contact_Contactpersoon', 'crm_Campagne_Campagne', 'Similarity']], reader)
@@ -26,41 +25,34 @@ def recommendations():
 
         algo = KNNBasic(sim_options={
             "name": "cosine",
-            "user_based": False,
+            "user_based":False,
         })
         algo.fit(trainset)
+
+        user_id = '01B45481-0877-E911-80FE-001DD8B72B62'
 
         user_campaigns = df[df['crm_Contact_Contactpersoon'] == user_id]['crm_Campagne_Campagne'].unique()
         full_dataset = [(user_id, campaign, 0) for campaign in df['crm_Campagne_Campagne'].unique() if campaign not in user_campaigns]
 
         user_predictions = algo.test(full_dataset)
+        print(user_predictions)
 
         pred_df = pd.DataFrame(user_predictions, columns=['uid', 'iid', 'r_ui', 'est', 'details'])
         pred_df = pred_df.sort_values(by='est', ascending=False)
 
+
         recommended_campaigns = pred_df[['uid', 'iid', 'est']]
         recommended_campaigns.columns = ['User', 'Campaign', 'Likelihood']
 
-        recommended_campaigns = pd.merge(recommended_campaigns, full, how='left', left_on='Campaign', right_on='crm_Campagne_Campagne')
+        recommended_campaigns = pd.merge(recommended_campaigns, full, how='left', left_on= 'Campaign', right_on='crm_Campagne_Campagne')
         recommended_campaigns = recommended_campaigns[['User', 'Campaign', 'crm_Campagne_Naam_in_email', 'Likelihood']]
         recommended_campaigns = recommended_campaigns.drop_duplicates(subset='Campaign')
 
-        ground_truth = [pred.r_ui for pred in user_predictions]
-
-        mse = mean_squared_error(ground_truth, [pred.est for pred in user_predictions])
-        rmse = np.sqrt(mse)
-
-        # Export results to a text file
-        metrics_results = f"Mean Squared Error (MSE): {mse}\n"
-        metrics_results += f"Root Mean Squared Error (RMSE): {rmse}\n"
-
-        file_path = f'user_recommendations_{user_id}.txt'
-        with open(file_path, 'w') as file:
-            file.write(metrics_results)
-
-        # Return the results
+        file_path = f'user_recommendations_{user_id}.csv'
+        recommended_campaigns.to_csv(file_path, index=False)
+        
         return render_template('recommendations.html', user_id=user_id, recommendations=recommended_campaigns, file_path=file_path)
-
+    
     return render_template('recommendations.html')
 
 @app.route('/download_recommendations/<filename>')
--- compare.py
+++ compare.py
@@ -1,6 +1,80 @@
-from csv_diff import load_csv, compare
-diff = compare(
-    load_csv(open("user_recommendations_01B45481-0877-E911-80FE-001DD8B72B62_1.csv"), key="crm_Campagne_Naam_in_email"),
-    load_csv(open("user_recommendations_01B45481-0877-E911-80FE-001DD8B72B62.csv"), key="crm_Campagne_Naam_in_email")
-)
-print(diff)
+import pandas as pd
+from surprise import Reader, Dataset
+from surprise.model_selection import train_test_split
+from surprise import KNNBasic
+from surprise import accuracy
+from sklearn.metrics import average_precision_score, mean_squared_error, mean_absolute_error, precision_score, recall_score, f1_score, roc_auc_score
+import numpy as np
+from sklearn.preprocessing import binarize
+
+# Load the data
+df = pd.read_csv('test.csv')
+full = df
+df = df[['crm_Contact_Contactpersoon', 'crm_Campagne_Campagne', 'Similarity', 'crm_Campagne_Naam_in_email']]
+
+# Load data into Surprise
+reader = Reader(rating_scale=(0, 10))
+data = Dataset.load_from_df(df[['crm_Contact_Contactpersoon', 'crm_Campagne_Campagne', 'Similarity']], reader)
+
+# Split the data into train and test sets
+trainset, testset = train_test_split(data, test_size=0.2, random_state=42)
+
+# Train the model
+algo = KNNBasic(sim_options={
+    "name": "cosine",
+    "user_based": False,
+})
+algo.fit(trainset)
+
+# Specify the user ID
+user_id = '01B45481-0877-E911-80FE-001DD8B72B62'
+
+# Get the campaigns that the user has visited
+user_campaigns = df[df['crm_Contact_Contactpersoon'] == user_id]['crm_Campagne_Campagne'].unique()
+
+# Create a testset with all campaigns (including those visited by the user)
+full_testset = [(user_id, campaign, 0) for campaign in user_campaigns] + list(testset)
+
+# Make predictions
+predictions = algo.test(full_testset)
+
+# Extract predicted ratings
+predicted_ratings = [pred.est for pred in predictions]
+
+# Calculate metrics
+ground_truth = [pred.r_ui for pred in predictions]
+
+# Calculate MAP
+y_true = [1 if campaign in user_campaigns else 0 for (_, campaign, _) in full_testset]
+y_score = predicted_ratings
+map_score = average_precision_score(y_true, y_score)
+
+# Calculate MSE, RMSE, and MAE
+mse = mean_squared_error(ground_truth, predicted_ratings)
+rmse = np.sqrt(mse)
+mae = mean_absolute_error(ground_truth, predicted_ratings)
+
+# Binarize the ground truth for classification metrics
+# Binarize the ground truth for classification metrics
+threshold = 5  # You can adjust the threshold based on your preference
+y_true_binary = np.where(np.array(ground_truth) >= threshold, 1, 0)
+y_pred_binary = np.where(np.array(predicted_ratings) >= threshold, 1, 0)
+
+
+# Calculate precision, recall, f1-score, and ROC AUC
+precision = precision_score(y_true_binary, y_pred_binary)
+recall = recall_score(y_true_binary, y_pred_binary)
+f1 = f1_score(y_true_binary, y_pred_binary)
+roc_auc = roc_auc_score(y_true_binary, predicted_ratings)
+
+# Print all metrics
+print(f"Mean Average Precision (MAP): {map_score}")
+print(f"Mean Squared Error (MSE): {mse}")
+print(f"Root Mean Squared Error (RMSE): {rmse}")
+print(f"Mean Absolute Error (MAE): {mae}")
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+print(f"F1-Score: {f1}")
+print(f"ROC AUC: {roc_auc}")
+
+# Rest of your code...
